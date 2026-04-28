725202217a412626b5b2b21bbe8acd9a98fee863
a25albsalort <a25albsalort@inspedralbes.cat>
2026-04-28 08:30:41 +0200

    Update index.php redirecciones a la funcion llistar de cada usuario

--- php/index.php
+++ php/index.php
@@ -10,9 +10,9 @@
     <h1>GI3P Gestor Incidencias DAM</h1>
     <p>Selecció d'usuari</p>
     <div id = "menu">
-        <a href="llistar.php">Profesor</a>
-        <a href="llistar.php">Tecnic</a>
-        <a href="llistar.php">Administrador</a>
+        <a href="llistarProfessors.php">Profesor</a>
+        <a href="llistarTecnics.php">Tecnic</a>
+        <a href="llistarAdmin.php">Administrador</a>
     </div>
     
 </body>
