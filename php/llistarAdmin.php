75032ed88f1657b2f2d4d372a9aa47d8fbd5e4cc
Zihao <a25zihzhazha@inspedralbes.cat>
2026-04-30 08:57:43 +0200

    Asignar tecnics a incidencies creada

--- php/llistarAdmin.php
+++ php/llistarAdmin.php
@@ -3,6 +3,28 @@
 //Sempre volem tenir una connexió a la base de dades, així que la creem al principi del fitxer
 require_once 'connexio.php';
 // Un cop inclòs el fitxer connexio.php, ja podeu utilitzar la variable $conn per a fer les consultes a la base de dades.
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+
+    if (isset($_POST["tecnic"], $_POST["id"])){
+
+    // Obtenir el nom de la casa del formulari
+$tecnic = $_POST["tecnic"];
+$idIncidencia = $_POST["id"];
+    // Comprovar si el nom no està buit
+    // Si l'html està ben escrit això no podria passar en els usuaris normals
+    // Igualment SEMPRE s'ha de comprovar tot al backend ja que no tots els usuaris
+    // són "bones persones" i des de les web tools es pot canviar tot el front per exemple.
+
+    // Preparar la consulta SQL per inserir una nova casa
+
+
+    $sentencia2= $conn->prepare("UPDATE INCIDENCIA SET idTecnic = ? WHERE idIncidencia = ?");
+
+    $sentencia2->bind_param("ii", $tecnic, $idIncidencia);
+    $sentencia2->execute();
+    }
+}
+
 
 ?>
 <!DOCTYPE html>
@@ -19,7 +41,7 @@ require_once 'connexio.php';
     <?php
 
     // Consulta SQL per obtenir totes les files de la taula 'cases'
-    $sql = "SELECT idIncidencia, descripcio, fecha FROM INCIDENCIA";
+    $sql = "SELECT idIncidencia, descripcio, fecha, idTecnic FROM INCIDENCIA";
     $result = $conn->query($sql);
 
     // Comprovar si hi ha resultats
@@ -27,9 +49,21 @@ require_once 'connexio.php';
 
         // Llistar els resultats. ATENCIÓ, heu de construir el codi HTML d'una llista correctament
         while ($row = $result->fetch_assoc()) {
-            echo "<p>ID: " . $row["idIncidencia"] . " - Nom: " . htmlspecialchars($row["descripcio"]) . "";
-            echo " <a href='esborrar.php?id=" . $row["idIncidencia"] . "'>Esborrar</a></p>";
-        }
+            echo "<p>ID incidencia: " . $row["idIncidencia"] . "   --- Descripcio: " . htmlspecialchars($row["descripcio"]) . "";
+            echo "   --- ID tecnic asignat: " . $row["idTecnic"]. "";
+            echo " <a href='esborrar.php?id=" . $row["idIncidencia"] . "'>Esborrar</a>";
+            echo "<form method='POST' action='llistarAdmin.php'>";
+            echo "<select name='tecnic' id='tecnic' required>";
+            echo "<option value='' selected>-- Asignar tecnic--</option>";
+            echo "<option value='1'>Juan</option>";
+            echo "<option value='2'>Alex</option>";
+            echo "<option value='3'>Luis</option>";
+            echo "</select>";     
+            echo "<input type='hidden' name='id' value='" . $row["idIncidencia"] . "'>";
+            echo "<input type='submit' value='Asignar'>";
+            echo "</form>";
+            echo "</p>";
+            }
 
     } else {
         echo "<p>No hi ha dades a mostrar.</p>";
