64a7edb76a1446659b0241abf8cebe8efd801431
Zihao <a25zihzhazha@inspedralbes.cat>
2026-05-04 10:47:19 +0200

    Afegir llistat d'incidencies sense tecnic asignat y d'incidencies obertes

--- php/llistarAdmin.php
+++ php/llistarAdmin.php
@@ -37,7 +37,8 @@ $idIncidencia = $_POST["id"];
 </head>
 
 <body>
-    <h1>Llistat de cases</h1>
+    <h1>Panell administrador</h1>
+    <h2>LLista d'incidencies sense tecnic asignat</h2>
     <?php
 
     // Consulta SQL per obtenir totes les files de la taula 'cases'
@@ -69,15 +70,88 @@ $idIncidencia = $_POST["id"];
         echo "<p>No hi ha dades a mostrar.</p>";
     }
 
+
+    ?>
+
+<p>---------------------------------------------------------------------------------------</p>
+<h2>LLista d'incidencies sense tecnic asignat</h2>
+    <?php
+
+    // Consulta SQL per obtenir totes les files de la taula 'cases'
+    $sql = "SELECT idIncidencia, descripcio, fecha, idTecnic FROM INCIDENCIA WHERE idTecnic IS NULL";
+    $result = $conn->query($sql);
+
+    // Comprovar si hi ha resultats
+    if ($result->num_rows > 0) {
+
+        // Llistar els resultats. ATENCIÓ, heu de construir el codi HTML d'una llista correctament
+        while ($row = $result->fetch_assoc()) {
+            echo "<p>ID incidencia: " . $row["idIncidencia"] . "   --- Descripcio: " . htmlspecialchars($row["descripcio"]) . "";
+            echo "   --- ID tecnic asignat: " . $row["idTecnic"]. "";
+            echo " <a href='esborrar.php?id=" . $row["idIncidencia"] . "'>Esborrar</a>";
+            echo "<form method='POST' action='llistarAdmin.php'>";
+            echo "<select name='tecnic' id='tecnic' required>";
+            echo "<option value='' selected>-- Asignar tecnic--</option>";
+            echo "<option value='1'>Juan</option>";
+            echo "<option value='2'>Alex</option>";
+            echo "<option value='3'>Luis</option>";
+            echo "</select>";     
+            echo "<input type='hidden' name='id' value='" . $row["idIncidencia"] . "'>";
+            echo "<input type='submit' value='Asignar'>";
+            echo "</form>";
+            echo "</p>";
+            }
+
+    } else {
+        echo "<p>No hi ha dades a mostrar.</p>";
+    }
+    ?>
+
+
+
+
+<p>---------------------------------------------------------------------------------------</p>
+<h2>LLista d'incidencies obertes</h2>
+    <?php
+
+    // Consulta SQL per obtenir totes les files de la taula 'cases'
+    $sql = "SELECT idIncidencia, descripcio, fecha, idTecnic FROM INCIDENCIA WHERE dataFinalitzacio IS NULL";
+    $result = $conn->query($sql);
+
+    // Comprovar si hi ha resultats
+    if ($result->num_rows > 0) {
+
+        // Llistar els resultats. ATENCIÓ, heu de construir el codi HTML d'una llista correctament
+        while ($row = $result->fetch_assoc()) {
+            echo "<p>ID incidencia: " . $row["idIncidencia"] . "   --- Descripcio: " . htmlspecialchars($row["descripcio"]) . "";
+            echo "   --- ID tecnic asignat: " . $row["idTecnic"]. "";
+            echo " <a href='esborrar.php?id=" . $row["idIncidencia"] . "'>Esborrar</a>";
+            echo "<form method='POST' action='llistarAdmin.php'>";
+            echo "<select name='tecnic' id='tecnic' required>";
+            echo "<option value='' selected>-- Asignar tecnic--</option>";
+            echo "<option value='1'>Juan</option>";
+            echo "<option value='2'>Alex</option>";
+            echo "<option value='3'>Luis</option>";
+            echo "</select>";     
+            echo "<input type='hidden' name='id' value='" . $row["idIncidencia"] . "'>";
+            echo "<input type='submit' value='Asignar'>";
+            echo "</form>";
+            echo "</p>";
+            }
+
+    } else {
+        echo "<p>No hi ha dades a mostrar.</p>";
+    }
+
     // Tancar la connexió
     $conn->close();
     ?>
 
+
+
     <div id="menu">
         <hr>
-        <p><a href="index.php">Portada</a> </p>
-        <p><a href="llistar.php">Llistar</a></p>
-        <p><a href="crear.php">Crear</a></p>
+        <p><a href="index.php">Tornar</a></p>
     </div>
 
 </body>
