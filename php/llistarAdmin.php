1f97132823425749e3d9ee47ef5f488affb15835
Zihao <a25zihzhazha@inspedralbes.cat>
2026-05-05 11:37:36 +0200

    Metodo que recibe el id  assosiado al nombre  de los campos necesarios para modificar, automaticamente en vez de manualmente

--- php/llistarAdmin.php
+++ php/llistarAdmin.php
@@ -3,27 +3,6 @@
 //Sempre volem tenir una connexió a la base de dades, així que la creem al principi del fitxer
 require_once 'connexio.php';
 // Un cop inclòs el fitxer connexio.php, ja podeu utilitzar la variable $conn per a fer les consultes a la base de dades.
-if ($_SERVER["REQUEST_METHOD"] === "POST") {
-
-    if (isset($_POST["tecnic"], $_POST["id"])){
-
-    // Obtenir el nom de la casa del formulari
-$tecnic = $_POST["tecnic"];
-$idIncidencia = $_POST["id"];
-    // Comprovar si el nom no està buit
-    // Si l'html està ben escrit això no podria passar en els usuaris normals
-    // Igualment SEMPRE s'ha de comprovar tot al backend ja que no tots els usuaris
-    // són "bones persones" i des de les web tools es pot canviar tot el front per exemple.
-
-    // Preparar la consulta SQL per inserir una nova casa
-
-
-    $sentencia2= $conn->prepare("UPDATE INCIDENCIA SET idTecnic = ? WHERE idIncidencia = ?");
-
-    $sentencia2->bind_param("ii", $tecnic, $idIncidencia);
-    $sentencia2->execute();
-    }
-}
 
 
 ?>
@@ -54,16 +33,6 @@ $idIncidencia = $_POST["id"];
             echo "   --- ID tecnic asignat: " . $row["idTecnic"]. "";
             echo " <a href='esborrar.php?id=" . $row["idIncidencia"] . "'>Esborrar</a>";
             echo " <a href='modificarIncidencia.php?id=" . $row["idIncidencia"] . "'>Modificar</a>";
-            echo "<form method='POST' action='llistarAdmin.php'>";
-            echo "<select name='tecnic' id='tecnic' required>";
-            echo "<option value='' selected>-- Asignar tecnic--</option>";
-            echo "<option value='1'>Juan</option>";
-            echo "<option value='2'>Alex</option>";
-            echo "<option value='3'>Luis</option>";
-            echo "</select>";     
-            echo "<input type='hidden' name='id' value='" . $row["idIncidencia"] . "'>";
-            echo "<input type='submit' value='Asignar'>";
-            echo "</form>";
             echo "</p>";
             }
 
